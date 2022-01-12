5e045a54d1924e26751e56ee674b95a51ab8777f
TT <theodore.martin@student.junia.com>
2022-01-12 08:20:01 +0100

    fixed login form going away, and made the login working, with placeholder function as sender

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -12,19 +12,25 @@ export class NavbarComponent implements OnInit {
 
   constructor(private route: ActivatedRoute) {}
 
+  closingFunc = (event: MouseEvent): void => {
+    // If user either clicks X button OR clicks outside the modal window, then close modal
+    if (event != null && event.target != null) {
+      const element = event.target as Element;
+      console.log(event.target);
+      if((element.matches(".close") || element.matches(".submitLogin") || !element.closest(".loginContent")) && !element.matches(".navBut")) {
+        (document.getElementById("loginModal") as HTMLElement).style.display = "none";
+        document.removeEventListener("click", this.closingFunc);
+        console.log("removed");
+      }
+    }
+  }
+
   loginModalClick() {
     (document.getElementById("loginModal") as HTMLElement).style.display = "block";
-    /*document.addEventListener(
+    document.addEventListener(
       "click",
-      function(event) {
-        // If user either clicks X button OR clicks outside the modal window, then close modal by calling closeModal()
-        if (event != null && event.target != null && (document.getElementById("loginModal") as HTMLElement).style.display != "none") {
-          const element = event.target as Element;
-          if(element.matches(".close") || !element.closest(".modal")) {
-            (document.getElementById("loginModal") as HTMLElement).style.display = "none";
-          }
-        }
-      }, false);*/
+      this.closingFunc,
+      false);
   }
 
   ngOnInit(): void {
